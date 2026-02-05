2f7e2d9720371220edfcb848343b7c1a8888fce9
karolziober <karolziober@gmail.com>
2026-02-06 00:17:44 +0200

    update

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
 "use strict";
 
-// DATA FETCHING
-async function loadData() {
-  const response = await fetch("data.json");
-  const data = await response.json();
-  return data;
-}
-const data = await loadData();
+class Dashboard {
+  constructor() {
+    this.activities = document.querySelector(".activities-container");
+    this.timeFrames = document.querySelector(".time-frame-nav");
+    this.data = null;
+    this.periodLabels = {
+      daily: "Previously",
+      weekly: "Last Week",
+      monthly: "Last Month",
+    };
+  }
 
-// SELECTORS
-const activities = document.querySelector(".activities-container");
-const timeFrames = document.querySelector(".time-frame-nav");
+  async init() {
+    await this.loadData();
+    this.renderCards("daily");
+    this.handleClick();
+  }
 
-// HELPER FUNCTION
-const renderCards = function (timeframe) {
-  activities.innerHTML = "";
-  data.forEach((item) => {
-    activities.insertAdjacentHTML(
-      "beforeend",
-      `<div class="activity-card">
-          <div class="card-banner"></div>
+  async loadData() {
+    const response = await fetch("data.json");
+    this.data = await response.json();
+  }
+
+  renderCards(timeframe) {
+    this.activities.innerHTML = "";
+    this.data.forEach((item) => {
+      this.activities.insertAdjacentHTML(
+        "beforeend",
+        `<div class="activity-card" >
+          <div class="card-banner" data-category="${item.title.toLowerCase().replace(" ", "-")}">
+          <img src="images/icon-${item.title.toLowerCase().replace(" ", "-")}.svg" alt="icon-${item.title.toLowerCase().replace(" ", "-")}">
+          </div>
           <div class="card-body">
             <div class="card-title">${item.title}</div>
+            <div class="time-section">
             <div class="time">${item.timeframes[timeframe].current}hrs</div>
-            <div class="time-previous">Previous - ${item.timeframes[timeframe].previous}hrs</div>
+            <div class="time-previous">${this.periodLabels[timeframe]} - ${item.timeframes[timeframe].previous}hrs</div>
+            </div>
           </div>
         </div>`,
-    );
-  });
-};
-renderCards("daily");
-
-// EVENT DELEGATION
-timeFrames.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (e.target.tagName === "BUTTON") {
-    const timeFrame = e.target.dataset.timeframe;
-    renderCards(timeFrame);
+      );
+    });
   }
-});
+
+  handleClick() {
+    this.timeFrames.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (e.target.tagName === "BUTTON") {
+        const timeFrame = e.target.dataset.timeframe;
+        this.renderCards(timeFrame);
+      }
+    });
+  }
+}
+
+const app = new Dashboard();
+await app.init();
